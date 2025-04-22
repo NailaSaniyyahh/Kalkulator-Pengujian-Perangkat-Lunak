8dbc49cc77d82fefc5b765bddb64cb513b4e1ee7
nailaanai <nailasaniyyah81@gmail.com>
2025-04-22 22:52:02 +0700

    add modul validasi - naila

--- app/src/main/java/org/kalkulator/InputValidation.java
+++
@@ -1,5 +0,0 @@
-package org.kalkulator;
-
-public class InputValidation {
-
-}
---
+++ app/src/main/java/org/kalkulator/Validation.java
@@ -0,0 +1,33 @@
+package org.kalkulator;
+
+public class Validation {
+
+    public static double validasiInputAngka(String input, String numBerapa) {
+        try {
+            return Double.parseDouble(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Error: " + numBerapa + " bukan angka.");
+        }
+    }
+
+    public static void validasiInput(int num1, int num2, char operator){
+        if (num1 < -32768 || num1 > 32767){
+            throw new IllegalArgumentException("Error: num1 tidak berada pada rentang.");
+        }
+        if (num2 < -32768 || num2 > 32767){
+            throw new IllegalArgumentException("Error: num2 tidak berada pada rentang.");
+        }
+    }
+
+    public static void validasiOperator(String operator){
+        if (!operator.equals("+") && !operator.equals("-") && !operator.equals("*") && !operator.equals("/")) {
+            throw new IllegalArgumentException( "Error: operator tidak valid");
+        }
+    }
+
+    public static void validasiPembagi(int num2, char operator) {
+        if (operator == '/' && num2 == 0) {
+            throw new IllegalArgumentException("Error: pembagi tidak boleh nol");
+        }
+    }
+}
