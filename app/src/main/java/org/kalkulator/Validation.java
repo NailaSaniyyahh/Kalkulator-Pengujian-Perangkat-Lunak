f4a852be14217c33fe1a2a61153a40ef72f0366e
RahmaDivina <rahma.divina.tif422@polban.ac.id>
2025-04-29 11:29:45 +0700

    modularize the Main.java and adjust with Calculate.java and Validation.java

--- app/src/main/java/org/kalkulator/Validation.java
+++ app/src/main/java/org/kalkulator/Validation.java
@@ -12,10 +12,10 @@ public class Validation {
 
     public static void validasiInput(int num1, int num2, char pilihan){
         if (num1 < -32768 || num1 > 32767){
-            throw new IllegalArgumentException("Error: num1 tidak berada pada rentang.");
+            throw new IllegalArgumentException("Error: Bilangan Pertama tidak berada pada rentang.");
         }
         if (num2 < -32768 || num2 > 32767){
-            throw new IllegalArgumentException("Error: num2 tidak berada pada rentang.");
+            throw new IllegalArgumentException("Error: Bilangan Kedua tidak berada pada rentang.");
         }
     }
 
@@ -26,7 +26,7 @@ public class Validation {
     }
 
     public static void validasiPembagi(int num2, char pilihan) {
-        if (pilihan == '/' && num2 == 0) {
+        if (pilihan == '4' && num2 == 0) {
             throw new IllegalArgumentException("Error: pembagi tidak boleh nol");
         }
     }
