b699f999498ef973cb9fe130832c0f13606127f0
nailaanai <nailasaniyyah81@gmail.com>
2025-05-03 18:31:08 +0700

    add test script for validation class

--- app/src/test/java/org/kalkulator/ValidationTest.java
+++ app/src/test/java/org/kalkulator/ValidationTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 
 public class ValidationTest {
 
-	//TC01
+	//TC01:Menguji input pertama bukan berupa angka yaitu abjad
 	@Test
     public void testvalidasiInputAngka1() {
         String input = "yyy";  
@@ -17,48 +17,68 @@ public class ValidationTest {
         assertEquals("Error: Input bukan angka.", thrown.getMessage());
     }
 
-	//TC02
+	//TC02:Menguji input valid berupa angka pada 
 	@Test
     public void testvalidasiInputAngka2() {
         String input1 = "2.7"; 
-		String input2 = "yyy";  
-        // IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> {
-        //     Validation.validasiInputAngka(input);
-        // });
-        // assertEquals("Error: Input bukan angka.", thrown.getMessage());
-		double num1 = Validation.validasiInputAngka(input1);
-        assertEquals(2.7, num1, "num1 harus valid dan dikembalikan sebagai 2.7");
+		String input2 = "3.0";  
 
-        IllegalArgumentException thrown2 = assertThrows(IllegalArgumentException.class, () -> {
-            Validation.validasiInputAngka(input2);  // Validasi untuk num2
-        });
-        assertEquals("Error: Input bukan angka.", thrown2.getMessage());
+		double result1 = Validation.validasiInputAngka(input1);
+        double result2 = Validation.validasiInputAngka(input2);
+
+        assertEquals(2.7, result1);
+        assertEquals(3.0, result2);
     }
 	
-	//TC03
-	@Test
-    void testvalidasiInput1() {
-        double num1 = 23;     
-        double num2 = 908765;    
+	//TC03:Menguji input kedua yang berupa angka dengan kondisi tidak berada pada rentang -32,768 hingga 32,767
+    @Test
+    public void testvalidasiInputNum2Invalid() {
+        double input1 = 23; 
+        double input2 = 908765;  
 
-        // Memastikan IllegalArgumentException dilemparkan untuk num2
         IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> {
-            Validation.validasiInput(num1, num2);
+            Validation.validasiInput(input1, input2);
         });
+        
         assertEquals("Error: Bilangan Kedua tidak berada pada rentang.", thrown.getMessage());
     }
 
-	//TC04
-	@Test
-    void testvalidasiInput2() {
-        double num1 = 908765;     
-        // double num2 = null;    
+    //TC04:Menguji input pertama yang berupa angka dengan kondisi tidak berada pada rentang -32,768 hingga 32,767
+    @Test
+	public void testvalidasiInputNum1Invalid() {
+        double input1 = -56789; 
+        double input2 = 23;  
 
-        // Memastikan IllegalArgumentException dilemparkan untuk num2
         IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> {
-            // Validation.validasiInput(num1, num2);
+            Validation.validasiInput(input1, input2);
         });
-        assertEquals("Error: Bilangan Kedua tidak berada pada rentang.", thrown.getMessage());
+        
+        assertEquals("Error: Bilangan Pertama tidak berada pada rentang.", thrown.getMessage());
+    }
+
+    //TC05:Menguji input opsi operator bukan berupa operasi basic +,-,*,/
+    @Test
+    public void testvalidasiOperatorInvalid(){
+        char pilihan = '7';
+
+        IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> {
+            Validation.validasiOperator(pilihan);
+        });
+        
+        assertEquals("Error: operator tidak valid", thrown.getMessage());
+    }
+
+    //TC06:Menguji input angka pembagi sama dengan nol (0)
+    @Test
+    public void testvalidasiPembagiInvalid() {
+        double num2 = 0;  
+        char pilihan = '4';  
+
+        IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> {
+            Validation.validasiPembagi(num2, pilihan);
+        });
+        
+        assertEquals("Error: pembagi tidak boleh nol", thrown.getMessage());
     }
 }
 
